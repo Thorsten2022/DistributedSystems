4a4730fe00746771c205e59bc85e66148687f816
rtyawegn <yannik.wegner@stud.hs-ruhrwest.de>
2022-02-07 09:36:50 +0100

    new project

---
+++ src/main/java/de/hrw/dsalab/distsys/WavesProjekt/WavesProjektApplication.java
@@ -0,0 +1,13 @@
+package de.hrw.dsalab.distsys.WavesProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavesProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavesProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hrw/dsalab/distsys/WavesProjekt/WavesProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hrw.dsalab.distsys.WavesProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavesProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
